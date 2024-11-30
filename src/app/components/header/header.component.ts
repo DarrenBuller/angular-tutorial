dd9c1cebcd052487a082f637398b7cda129b5f1d
Darren Buller <stash-taco-remake@duck.com>
2024-11-30 17:18:09 +0000

    Basic data binding

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   imports: [],
-  template: `
-    <p>
-      header works!
-    </p>
-  `,
+  templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  title: string = 'This loaded dynamically';
+  imageUrl: string = 'https://www.bax-shop.co.uk/templates/baxshop/img/logo_21jaar_en.svg';
+  altImageUrl: string = 'Bax Music established 21 years';
+  isButtonDisabled: boolean = false;
+  isButtonActive: boolean = true;
+  buttonDisable: string = 'true'
 }
