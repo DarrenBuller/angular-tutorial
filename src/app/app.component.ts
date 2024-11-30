7a923c174933dd3460169c50f5389ac7b07e79a4
Darren Buller <stash-taco-remake@duck.com>
2024-11-30 20:03:25 +0000

    Add examples of event handeling and data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "@components/navbar/navbar.component";
 import { HeaderComponent } from "@components/header/header.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeaderComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, FormsModule],
   template: `
     <app-header/>
     <app-navbar/>
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
   title: string = 'This loaded dynamically';
   imageUrl: string = 'https://www.bax-shop.co.uk/templates/baxshop/img/logo_21jaar_en.svg';
   altImageUrl: string = 'Bax Music established 21 years';
   isButtonDisabled: boolean = false;
   isButtonActive: boolean = true;
-  buttonDisable: string = 'true'
+  buttonDisable: string = 'true';
+  userName: string =  'Joe Smith'
+  textValue: string = 'One Way'
+
+  buttonClick() {
+    console.log('Button Clicked')
+  }
+
+  keyEnter(event:any){
+    console.log(event.keyCode)
+    if (event.keyCode == 13) {
+      console.log('enter')
+      console.log(this.userName)
+    }
+  }
+  keyUpFiltering() {
+    console.log('key up enter')
+  }
+  keyUserFiltering(user: HTMLInputElement) {
+    console.log(user.value)
+  }
+  updateUserName(userName: HTMLInputElement) {
+    console.log(userName.value)
+    } 
 }
