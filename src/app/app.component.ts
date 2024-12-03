614411a8d7abad2233d55cc7e2621f38d569f534
Darren Buller <stash-taco-remake@duck.com>
2024-12-03 15:15:11 +0000

    Using message events from the child to parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { PostsListComponent } from "./components/posts-list/posts-list.component";
@@ -12,7 +12,9 @@ import { PostsListComponent } from "./components/posts-list/posts-list.component
     <app-navbar/>
     -->
     <h1>-----{{postListTitle}}-----</h1>
-    <app-posts-list [postTitle]="postListTitle" [isLoggedIn]="appIsLoggedIn"/>
+    <app-posts-list (messageEvent)="receiveButtonMessage($event)" [postTitle]="postListTitle" [isLoggedIn]="appIsLoggedIn"/>
+    <h2>Child message after view init: {{message}}</h2>
+    <h2>Child message from event: {{messageFromChildEvent}}</h2>
     <!--
     <h1>Hello, {{title}}!</h1>
     <h2 class="text-5xl font-bold underline">Tailwind works!</h2>
@@ -22,8 +24,24 @@ import { PostsListComponent } from "./components/posts-list/posts-list.component
   `,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+
   title = 'angular-tutorial';
   postListTitle: string = 'Some title fffffff';
   appIsLoggedIn: boolean = true;
+  @ViewChild(PostsListComponent) childMessage: any;
+  message: string = '';
+  messageFromChildEvent: string = '';
+
+  constructor(){
+    console.log('constructor childMessage: ' + this.childMessage);
+  }
+  ngAfterViewInit(): void {
+    console.log(this.childMessage);
+    this.message = this.childMessage.childMessage;
+  }
+  receiveButtonMessage(message: any) {
+    console.log(message);
+    this.messageFromChildEvent = message;
+  }
 }
--- src/app/components/posts-list/posts-list.component.ts
+++ src/app/components/posts-list/posts-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-posts-list',
@@ -7,10 +7,22 @@ import { Component, Input } from '@angular/core';
     <p>
       posts-list works! {{postTitle}} {{isLoggedIn}}
     </p>
+    <p><label>Post Title from parent: </label><input type="text" (ngModel)="postTitle" /></p>
+    <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+    (click)="sendMessageToParent()">Send message to Parent</button>
   `,
   styleUrl: './posts-list.component.css'
 })
 export class PostsListComponent {
   @Input() postTitle: string = '';
   @Input() isLoggedIn: boolean = false;
+  @Output() messageEvent = new EventEmitter();
+  childMessage: string = 'a message from child component';
+  parentMessage: string = 'Button clicked message for parent';
+
+  sendMessageToParent() {
+    console.log('sendMessageToParent ' + this.parentMessage);
+
+    this.messageEvent.emit(this.parentMessage);
+  }
 }
